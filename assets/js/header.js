e29e1adf7153d336edb8640f41127d66f6c9cfe3
aliceasuna94 <59562911+aliceasuna94@users.noreply.github.com>
2020-09-14 16:09:38 +0800

    Improve loading story speed

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,19 @@
-$(document).ready(function(){
-    
+$(document).ready(async function(){
+
     var ex = localStorage.getItem('reave-login');
     var drr = localStorage.getItem('reave-address');
-    
+
+    var profile = await getNameProfile(drr);
+
     if(ex === 'true'){
          document.getElementById('btnloghead').style.display = 'none';
          document.getElementById('dropdownhead').style.display = 'block';
-         document.getElementById('dropdownheadlink').innerText = drr.slice(0, 15);
+         document.getElementById('dropdownheadlink').innerText = profile;
      }else{
          document.getElementById('btnloghead').style.display = 'block';
          document.getElementById('dropdownhead').style.display = 'none';
      }
-    
+
 });
 
 function started(){
@@ -21,4 +23,38 @@ function started(){
 function toMyAuthor(){
     var drr = localStorage.getItem('reave-address');
     window.location.href = 'author.html?'+drr;
+}
+
+
+async function getNameProfile(e){
+    try {
+          const contx = await arweave.arql({
+              op: "and",
+              expr1: {
+                op: "equals",
+                  expr1: "from",
+                  expr2: e
+              },
+              expr2: {
+                  op:"equals",
+                  expr1:"Reave-Type",
+                  expr2: "profile"
+
+              }
+        })
+
+           //console.log(contx);
+           if (contx.length > 0) {
+               const profileTx     = await arweave.transactions.get(contx[0]);
+               const profileTxData = profileTx.get('data', { decode: true, string: true });
+               //console.log(profileTxData);
+               let profileTxDatare = profileTxData.split('hcseu83h387svlnv8');
+               return profileTxDatare[0];
+
+           }else {
+               //console.log('Profil not set');
+           }
+    } catch (e) {
+        return false;
+    }
 }
--- assets/js/story.js
+++ assets/js/story.js
@@ -1,10 +1,14 @@
 getStory();
-
+let mongoData = [];
 async function getStory() {
+    //showloading
+    document.getElementById('readspinner').style.display = "block";
+    document.getElementById('loadmore').style.display = 'none';
+
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
         targetUrl = 'https://reavedb8.herokuapp.com/story';
 
-    let mongoData = await fetch(proxyUrl + targetUrl)
+    mongoData = await fetch(proxyUrl + targetUrl)
       .then(blob => blob.json())
       .then(data => {
         return data;
@@ -14,24 +18,66 @@ async function getStory() {
         return e;
       });
 
-        domAppend(mongoData);
+      mongoData = mongoData.sort(dynamicSort("-stamp"));
+      sessionStorage.setItem("pagination", "0");
+      sendDataToDom();
 
 }
 
 
 //Fungsi untuk menampilkan atau Append dari Array
-async function domAppend(e) {
-      e = e.sort(dynamicSort("-stamp"));
-      e.forEach(function (item, value){
+async function sendDataToDom() {
 
-        console.log(item);
-          var toAppend = '<div class="col-sm-6 item" id="storyitem"> <div class="row boxarticle"> <div class="col-md-12 col-lg-5 imagebox"><a href="read.html?'+item.storyid+'"><img class="img-fluid" id="'+item.storyid+'" src="loadingimages.svg" /></a></div> <div class="col rightbox"><a id="storytitle" class="titlelink boxtitle" href="read.html?'+item.storyid+'">'+item.title+'</a> <div class="author" style="margin-top: 10px;"><i class="fa fa-user-o float-left" aria-hidden="true" style="margin: 3px;"></i> <a href="author.html?'+item.storyid+'" style="color:black;"><h5 class="float-left name storyauthor" id="storyauthor">'+item.author+'</h5></a> <p id="storydate" class="name mydatelist"> '+item.stamp+'</p> </div> <p id="storydesc" class="description boxdesc">'+item.desc+'</p><a class="badge badge-dark text-white" id="storycategory">'+item.category+'</a> <a class="badge badge-success text-white" id="tipped">'+item.tip+' AR</a></div> </div> </div>';
-          $('#storylist').append(toAppend);
+    var u = sessionStorage.getItem("pagination");
+    var a = Number(u) + 6;
 
-      });
+    e = mongoData.slice(u,a);
+    await domAppand(e).then(function (data) {
+          domThumbnail(e);
+    });
+
+    sessionStorage.setItem("pagination", a);
+
+
+    document.getElementById('readspinner').style.display = "none";
+
+
+    if (a >= mongoData.length) {
+          document.getElementById('loadmore').style.display = 'none';
+    }else {
+      document.getElementById('loadmore').style.display = 'block';
+    }
 
 }
 
+//dom story
+async function domAppand(e) {
+
+  //Perulangan
+  e.forEach(function (item, value){
+
+      var times = item.stamp.substring(0, 10);
+      var date = moment.unix(times).format("MMM Do YY");
+
+      var toAppend = '<div class="col-sm-6 item" id="storyitem"> <div class="row boxarticle"> <div class="col-md-12 col-lg-5 imagebox"><a href="read.html?'+item.storyid+'"><img class="img-fluid" id="'+item.storyid+'" src="loadingimages.svg" /></a></div> <div class="col rightbox"><a id="storytitle" class="titlelink boxtitle" href="read.html?'+item.storyid+'">'+item.title+'</a> <div class="author" style="margin-top: 10px;"><i class="fa fa-user-o float-left" aria-hidden="true" style="margin: 3px;"></i> <a href="author.html?'+(item.storyid).slice(0,43)+'" style="color:black;"><h5 class="float-left name storyauthor" id="storyauthor">'+item.author+'</h5></a> <p id="storydate" class="name mydatelist"> '+date+'</p> </div> <p id="storydesc" class="description boxdesc">'+item.desc+'</p><a class="badge badge-dark text-white" id="storycategory">'+item.category+'</a> <a class="badge badge-success text-white" id="tipped">'+item.tip+' AR</a></div> </div> </div>';
+      $('#storylist').append(toAppend);
+
+      console.log('Dom Data');
+
+  });
+}
+
+//Dom Thumbnail
+async function domThumbnail(e) {
+    e.forEach(async function (item, value){
+        console.log("DOm Thumbnail");
+        const p = await arweave.transactions.get(item.storytagtx);
+        const m = p.get('data', { decode: true, string: true });
+
+        $("#"+item.storyid).attr("src", m);
+    });
+
+}
 
 //Sort inbox function
 function dynamicSort(property) {
